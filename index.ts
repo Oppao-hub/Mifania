51bc123eaf16aa878fe8fda87afe81354efffa17
Bien <lopao0012@gmail.com>
2026-04-26 16:11:37 +0800

    refactor:add @ts-ignore

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
+//@ts-ignore
 import "./global.css"
 
 AppRegistry.registerComponent(appName, () => App);
