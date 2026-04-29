d6255e48973eb27f2be2976db158d8ab7952f0bf
Bien <lopao0012@gmail.com>
2026-04-29 09:12:28 +0800

    add: created a featureitems component

--- src/components/FeatureItem.tsx
+++ src/components/FeatureItem.tsx
@@ -1,23 +1,19 @@
 import { View, Text, TouchableOpacity } from 'react-native';
 import React from 'react';
-import DynamicIcon from './DynamicIcon';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 interface FeatureItemProps {
   name: string;
-  iconName: string;
   onPress?: () => void;
 }
 
-const FeatureItem: React.FC<FeatureItemProps> = ({ name, iconName, onPress }) => {
+const FeatureItem: React.FC<FeatureItemProps> = ({ name, onPress }) => {
   return (
     <TouchableOpacity 
       onPress={onPress} 
       className="items-center mr-6"
       activeOpacity={0.7}
     >
-      <View className="w-16 h-16 rounded-full bg-light-gray items-center justify-center mb-2">
-        <DynamicIcon name={iconName} size={24} color="#52622E" />
-      </View>
       <Text className="text-xs font-montserrat text-dark-gray">{name}</Text>
     </TouchableOpacity>
   )
