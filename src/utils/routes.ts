4e35cbb24548977db9d5ae64c90790f8f81ee18b
Bien <lopao0012@gmail.com>
2026-04-29 21:06:39 +0800

    add: include additional routes for wishlist, order, and product details

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -1,6 +1,9 @@
 export const ROUTES = {
     HOME: 'Home' as const,
     CART: 'Cart' as const,
+    WISHLIST: 'Wishlist' as const,
+    ORDER: 'Order' as const,
+    PRODUCT_DETAILS: 'ProductDetails' as const,
     PROFILE: 'Profile' as const,
     LOGIN: 'Login' as const,
     REGISTER: 'Register' as const,
