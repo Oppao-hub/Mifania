aa5ac93939d9a0af833944f93c6d3c29a02dc60e
Bien <lopao0012@gmail.com>
2026-05-04 19:43:37 +0800

    refactor: clean up App.tsx by removing unused toastConfig and imports

--- App.tsx
+++ App.tsx
@@ -1,70 +1,19 @@
 import React from 'react';
 import { View } from 'react-native';
 import { Provider } from 'react-redux';
-import { SafeAreaProvider } from 'react-native-safe-area-context';  
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Toast from 'react-native-toast-message';
+import { io } from "socket.io-client";  
 
-import Toast, { BaseToast, ErrorToast, InfoToast, ToastConfig } from 'react-native-toast-message'; 
 import configureStore from './src/app/store'; 
 import AppNavigation from './src/navigations';
+import { toastConfig } from './src/utils/toastConfig';
 
+const USER_ID = "1";
 const { store } = configureStore();
 
-const toastConfig: ToastConfig = {
-  success: (props) => (
-    <BaseToast
-      {...props}
-      style={{ borderLeftColor: '#52622E', backgroundColor: '#FFFFFF', borderRadius: 8, height: 70 }}
-      contentContainerStyle={{ paddingHorizontal: 15 }}
-      text1Style={{
-        fontSize: 16,
-        fontWeight: '700',
-        color: '#3F4C23',
-        fontFamily: 'Montserrat-Bold'
-      }}
-      text2Style={{
-        fontSize: 13,
-        color: '#6A7282',
-        fontFamily: 'Montserrat-Regular'
-      }}
-    />
-  ),
-  error: (props) => (
-    <ErrorToast
-      {...props}
-      style={{ borderLeftColor: '#DC3545', backgroundColor: '#FFFFFF', borderRadius: 8, height: 70 }}
-      text1Style={{
-        fontSize: 16,
-        fontWeight: '700',
-        color: '#DC3545',
-        fontFamily: 'Montserrat-Bold'
-      }}
-      text2Style={{
-        fontSize: 13,
-        color: '#6A7282',
-        fontFamily: 'Montserrat-Regular'
-      }}
-    />
-  ),
-  info: (props) => (
-    <InfoToast
-      {...props}
-      style={{ borderLeftColor: '#52622E', backgroundColor: '#FFFFFF', borderRadius: 8, height: 70 }}
-      text1Style={{
-        fontSize: 16,
-        fontWeight: '700',
-        color: '#3F4C23',
-        fontFamily: 'Montserrat-Bold'
-      }}
-      text2Style={{
-        fontSize: 13,
-        color: '#6A7282',
-        fontFamily: 'Montserrat-Regular'
-      }}
-    />
-  )
-};
-
 const App = () => {
+  
   return (
     <SafeAreaProvider>
       <Provider store={store}>
