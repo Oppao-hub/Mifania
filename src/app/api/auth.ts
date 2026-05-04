8042083ef5ff8d978ac85c7da86fab3f7e449c43
Bien <lopao0012@gmail.com>
2026-05-04 19:46:01 +0800

    feat: update API functions to use specific types for login and registration

--- src/app/api/auth.ts
+++ src/app/api/auth.ts
@@ -1,20 +1,20 @@
-import { AuthCredentials } from '../../types';
+import { LoginCredentials, RegisterCredentials, LoginResponse, RegisterResponse } from '../../utils/types';
 import { postRequest } from './client';
 
-export const userLoginApi = async (credentials: AuthCredentials) => {
-    return await postRequest("/login", credentials);
+export const userLoginApi = async (credentials: LoginCredentials) => {
+    return await postRequest<LoginResponse>("/login", credentials);
 };
 
 export const userGoogleLoginApi = async (idToken: string) => {
-    return await postRequest("/login/google", { idToken });
+    return await postRequest<LoginResponse>("/login/google", { idToken });
 };
 
-export const userRegisterApi = async (credentials: AuthCredentials) => {
+export const userRegisterApi = async (credentials: RegisterCredentials) => {
     const body = {
-        first_name: credentials.firstName || credentials.first_name,
-        last_name: credentials.lastName || credentials.last_name,
+        first_name: credentials.firstName,
+        last_name: credentials.lastName,
         email: credentials.email,
         password: credentials.password
     };
-    return await postRequest("/register", body);
+    return await postRequest<RegisterResponse>("/register", body);
 };
