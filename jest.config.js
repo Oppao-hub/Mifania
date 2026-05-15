ba53da0b49cbff5d2371c9c7e27d3e9f465cf9e7
Bien <lopao0012@gmail.com>
2026-05-15 23:21:34 +0800

    fix: update transformIgnorePatterns in jest.config.js for improved module handling

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   preset: 'react-native',
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|react-redux|@react-navigation|@react-native-community|react-native-safe-area-context|react-native-toast-message|nativewind|react-native-css-interop|@react-native-firebase|firebase|@firebase)/)',
+  ],
 };
