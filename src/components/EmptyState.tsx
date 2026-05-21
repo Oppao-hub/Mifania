8e40efd7de834360673e9e2f9ce436eb7c954ede
Bien <lopao0012@gmail.com>
2026-05-21 16:31:43 +0800

    refactor: remove unused Platform import from EmptyState component

--- src/components/EmptyState.tsx
+++ src/components/EmptyState.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Platform } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 interface EmptyStateProps {
