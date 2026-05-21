32f573f2c2272ce6ed01b4cc4458729600589955
Bien <lopao0012@gmail.com>
2026-05-21 16:27:20 +0800

    feat: enhance Redux store configuration with persistence and additional reducers

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,5 +1,9 @@
 import { legacy_createStore as createStore, applyMiddleware, combineReducers } from "redux";
 import createSagaMiddleware from "redux-saga";
+import { persistStore, persistReducer } from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import secureStorage from '../../utils/secureStorage';
+
 import { authReducer } from '../reducers/auth';
 import { productReducer } from '../reducers/product';
 import rootSaga from "../sagas";
@@ -7,27 +11,56 @@ import { categoryReducer } from "../reducers/category";
 import { subCategoryReducer } from "../reducers/subCategory";
 import { cartReducer } from "../reducers/cart";
 import { wishlistReducer } from "../reducers/wishlist";
+import { customerReducer } from "../reducers/customer";
+import { loyaltyReducer } from "../reducers/loyalty";
+import { orderReducer } from "../reducers/order";
+import { notificationReducer } from "../reducers/notification";
+import * as Types from '../actions';
 
 const sagaMiddleware = createSagaMiddleware();
 
-const rootReducer = combineReducers({
-    authentication: authReducer,
+const authPersistConfig = {
+    key: 'auth',
+    storage: secureStorage,
+};
+
+const rootPersistConfig = {
+    key: 'root',
+    storage: AsyncStorage,
+    whitelist: ['cart', 'wishlist'] // Persist cart and wishlist, auth is handled separately
+};
+
+const appReducer = combineReducers({
+    authentication: persistReducer(authPersistConfig, authReducer),
     product: productReducer,
     category: categoryReducer,
     subCategory: subCategoryReducer,
     cart: cartReducer,
     wishlist: wishlistReducer,
+    customer: customerReducer,
+    loyalty: loyaltyReducer,
+    order: orderReducer,
+    notification: notificationReducer,
 });
 
-const configureStore = () => {
-    const store = createStore(
-        rootReducer,
-        applyMiddleware(sagaMiddleware)
-    );
-
-    sagaMiddleware.run(rootSaga);
-
-    return { store };
+const rootReducer = (state: any, action: any) => {
+    if (action.type === Types.USER_LOGOUT) {
+        // clear local storage for auth since it's nested
+        secureStorage.removeItem('auth');
+        state = undefined;
+    }
+    return appReducer(state, action);
 };
 
-export default configureStore;
+const persistedReducer = persistReducer(rootPersistConfig, rootReducer);
+
+const store = createStore(
+    persistedReducer,
+    applyMiddleware(sagaMiddleware)
+);
+
+export const persistor = persistStore(store);
+
+sagaMiddleware.run(rootSaga);
+
+export default store;
