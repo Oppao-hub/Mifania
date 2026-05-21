e9196f9922b4e9c87ca978df3577f9e9e32aba22
Bien <lopao0012@gmail.com>
2026-05-21 16:22:57 +0800

    feat: consolidate saga imports and add watchLogout to rootSaga

--- src/app/sagas/index.ts
+++ src/app/sagas/index.ts
@@ -1,18 +1,26 @@
 import { all } from 'redux-saga/effects';
-import { watchLogin } from './auth';
-import { watchRegister } from './auth';
+import { watchLogin, watchRegister, watchLogout } from './auth';
 import { watchProduct } from './product';
 import { watchCategory } from './category';
 import { watchSubCategory } from './subCategory';
 import { watchCart } from './cart';
+import { watchSocket } from './socket';
+import { watchCustomer } from './customer';
+import { watchLoyalty } from './loyalty';
+import { watchOrder } from './order';
 
 export default function* rootSaga() {
     yield all([
         watchLogin(),
         watchRegister(),
+        watchLogout(),
         watchProduct(),
         watchCategory(),
         watchSubCategory(),
         watchCart(),
+        watchSocket(),
+        watchCustomer(),
+        watchLoyalty(),
+        watchOrder(),
     ]);
 }
