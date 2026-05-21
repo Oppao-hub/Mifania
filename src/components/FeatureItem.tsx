61a437335239bd8df4b70871b61d0d4dca3d98c0
Bien <lopao0012@gmail.com>
2026-05-21 16:33:34 +0800

    refactor: remove unused imports from FeatureItem component

--- src/components/FeatureItem.tsx
+++ src/components/FeatureItem.tsx
@@ -1,6 +1,5 @@
-import { View, Text, TouchableOpacity } from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
 import React from 'react';
-import Icon from 'react-native-vector-icons/Ionicons';
 
 interface FeatureItemProps {
   name: string;
