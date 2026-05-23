69c9256d9517876030ee165c74a550cd81b4bdd1
Bien <lopao0012@gmail.com>
2026-05-23 13:08:55 +0800

    refactor(api): centralize customer and endpoint resolution

--- src/app/api/customer.ts
+++ src/app/api/customer.ts
@@ -1,10 +1,11 @@
 import { Customer } from '../../utils/types';
+import { resolveCustomerEndpoint } from '../../utils/apiResource';
 import { getRequest, patchRequest } from './client';
 
-export const getCustomerApi = async (id: number, token: string) => {
-    return await getRequest<Customer>(`/customers/${id}`, token);
+export const getCustomerApi = async (customerRef: string | Customer | number, token: string) => {
+    return await getRequest<Customer>(resolveCustomerEndpoint(customerRef), token);
 };
 
-export const updateCustomerApi = async (id: number, data: Partial<Customer>, token: string) => {
-    return await patchRequest<Customer>(`/customers/${id}`, data, token);
+export const updateCustomerApi = async (customerRef: string | Customer | number, data: Partial<Customer>, token: string) => {
+    return await patchRequest<Customer>(resolveCustomerEndpoint(customerRef), data, token);
 };
