f50080abee7f1f2afce9d1f97a4ae235648f42ee
Bien <lopao0012@gmail.com>
2026-05-29 15:54:48 +0800

    refactor: replace Toast notifications in AlertMsg component with userFeedback utility functions for error, success, and info messages

--- src/components/AlertMsg/AlertMsg.tsx
+++ src/components/AlertMsg/AlertMsg.tsx
@@ -1,43 +1,26 @@
-import Toast from 'react-native-toast-message';
+import {
+  showBlockingError,
+  showBlockingInfo,
+  showBlockingSuccess,
+} from '../../utils/userFeedback';
 
 interface AlertMsgConfig {
-    title: string;
-    message: string;
+  title: string;
+  message: string;
 }
 
 const AlertMsg = {
-    customError: ({ title, message }: AlertMsgConfig) => {
-        Toast.show({
-            type: 'modalError',
-            text1: title,
-            text2: message,
-            position: 'top', 
-            topOffset: 0,
-            autoHide: false,
-        });
-    },
+  customError: ({ title, message }: AlertMsgConfig) => {
+    showBlockingError({ title, message });
+  },
 
-    customSuccess: ({ title, message }: AlertMsgConfig) => {
-        Toast.show({
-            type: 'modalSuccess',
-            text1: title,
-            text2: message,
-            position: 'top',
-            topOffset: 0,
-            autoHide: false,
-        });
-    },
+  customSuccess: ({ title, message }: AlertMsgConfig) => {
+    showBlockingSuccess({ title, message });
+  },
 
-    customInfo: ({ title, message }: AlertMsgConfig) => {
-        Toast.show({
-            type: 'modalInfo',
-            text1: title,
-            text2: message,
-            position: 'top',
-            topOffset: 0,
-            autoHide: false,
-        });
-    }
+  customInfo: ({ title, message }: AlertMsgConfig) => {
+    showBlockingInfo({ title, message });
+  },
 };
 
 export default AlertMsg;
